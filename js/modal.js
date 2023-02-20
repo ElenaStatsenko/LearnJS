d8e04dfa916b8729a68358e0c5c399150c46a882
Olena <kovalchuk.e.u@gmail.com>
2023-02-20 22:17:54 +0200

    сделал макет для JS

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+
+propName = "name";
+
+const newArr = [];
+
+for (const key of products) {
+  
+  if()
+ 
+  newArr.push(key.name);
+
+  
+}
+console.log(newArr)
+  
